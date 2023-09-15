931c8f0140d14aebe688f05101c97d9d8290306b
Alexandre RIERA <alexandre.riera1@viacesi.fr>
2023-09-15 20:23:31 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Good Food Ionic Version',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
